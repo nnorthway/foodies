4b2f1f631857c6e7aba0dd132dbf54e6da954b87
Nate Northway <nate@natenorthway.com>
2026-09-25 09:34:34 -0400

    Adding form & logic
    added form to share page
    
    added image uploader component
    
    init of actions.js, with saveMeal
    
    added saveMeal to meals.js, form handler
    
    added xss & slugify packages

---
+++ app/components/meals/imagepicker.jsx
@@ -0,0 +1,44 @@
+'use client'
+import { useRef, useState } from "react"
+import styles from "./imagepicker.module.css"
+import Image from "next/image"
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState()
+  const imageInput = useRef()
+  function handlePickClick() {
+    imageInput.current.click()
+  }
+  function handleImageChange(e) {
+    const file = e.target.files[0]
+    if (!file) {
+      setPickedImage(null)
+      return
+    }
+    const fileReader = new FileReader
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result)
+    }
+    fileReader.readAsDataURL(file)
+  }
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!pickedImage && <p>No image selected</p>}
+          {pickedImage && <Image src={pickedImage} alt="Your selected image" fill />}
+        </div>
+        <input 
+          className={styles.input}
+          type="file" 
+          id={name} 
+          accept="image/png, image/jpeg"
+          name={name} 
+          ref={imageInput} 
+          onChange={handleImageChange} 
+          required />
+        <button className={styles.button} onClick={handlePickClick}>Pick an Image</button>
+      </div>
+    </div>
+  )
+}
