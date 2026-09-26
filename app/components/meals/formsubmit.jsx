e269635986c964fc9c19423b0aed12c78159abbb
Nate Northway <nate@natenorthway.com>
2026-09-26 09:29:25 -0400

    updating component function name
    mistyped a capital letter, led to build errors.

--- app/components/meals/formsubmit.jsx
+++ app/components/meals/formsubmit.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { useFormStatus } from "react-dom"
-export default function formSubmit() {
+export default function FormSubmit() {
   const { pending } = useFormStatus()
   return (
     <button disabled={pending} type="submit">{pending ? "Submitting...":"Share Meal"}</button>
