dd4882cc73d30c3f40915a71c486ab9eb4f57b1d
Nate Northway <nate@natenorthway.com>
2026-09-26 09:31:08 -0400

    fixing more build errors
    theres gotta be a way to disable this

--- app/meals/not-found.js
+++ app/meals/not-found.js
@@ -2,7 +2,7 @@ export default function FourOhFour() {
   return (
     <main className="not-found">
       <h1>Meal Not Found</h1>
-      <p>Sorry, you landed on a page you weren't supposed to see.</p>
+      <p>Sorry, you landed on a page you weren&apos;t supposed to see.</p>
     </main>
   )
 }
--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
         <section className={styles.section}>
           <h2>How it works</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite recipes with the world. It's a place to discover new dishes, and to connect with other food lovers.
+            NextLevel Food is a platform for foodies to share their favorite recipes with the world. It&apos;s a place to discover new dishes, and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect with other food lovers.
@@ -33,7 +33,7 @@ export default function Home() {
         <section className={styles.section}>
           <h2>Why NextLevel Food?</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite recipes with the world. It's a place to discover new dishes, and to connect with other food lovers.
+            NextLevel Food is a platform for foodies to share their favorite recipes with the world. It&apos;s a place to discover new dishes, and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect with other food lovers.
